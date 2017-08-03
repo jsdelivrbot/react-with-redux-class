2d8c2367e5958b3cfb2476d9051985c77fa243ab
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-03 11:35:40 -0600

    code and notes ending at lecture 11

---
+++ ReduxSimpleStarter-master/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'; //Core library
+import ReactDOM from 'react-dom'; //ReactDOM is used when we're trying to put react stuff right into the dom
+
+// Create a new component. This component should produce some html
+// const means: no reassignment (App won't change) {as I'd expect const to mean}
+// => is ES6 syntax used as a function definer and is equivalent to:
+// const App = function() {...}
+const App = () => {
+  // HTML stuff here is actually JSX (subset of JS)
+  //  allows us to write html as javascript
+  //  webpack and babel will transpiling of code before it's run in browser
+  return <div>Hi!</div>;
+}
+
+//what does above look like as vanilla JS?
+// var _temporalUndefined = {};
+// var App = _temporalUndefined;
+
+// App = function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hi!"
+//   );
+// };
+
+// Note: <div /> or <App /> is valid JSX. This is considered an instance whereas Line 6 was just a class
+
+// Take this component's generated HTML and put it on the page (in DOM)
+// This requires an instance of the class which is why .render(App); doesn't work
+ReactDOM.render(<App />, document.querySelector('.container'));
+// 2nd parameter is where this gets rendered to
