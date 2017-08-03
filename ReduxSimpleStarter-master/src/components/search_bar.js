cb2d5cb84a13f4c418a2031202e5efacc3d2e5fb
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-03 14:37:19 -0600

    1-15 setting up search_bar as class component

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -1,9 +1,25 @@
-import React from 'react';
-// Still need React because our JSX below uses React.createElement
+import React, { Component } from 'react';
+// -Still need React because our JSX below uses React.createElement
+// -{} implies following line:
+// const Component = React.Component;
 
-const SearchBar = () => {
-  return <input />
+// this is called a functional component, because it uses a function
+// const SearchBar = () => {
+//   return <input />
+// }
+
+// the other type of component is a class component:
+// used when we want some type of record keeping and aware of itself and what's happened since it was rendered
+class SearchBar extends Component {
+  //syntax of class method
+  render() {
+    // necessary method needed for all class components
+    // this method must also return some JSX
+    return <input />;
+  }
 }
+
+// When to use functional vs class component? It's challenging/dependant. Try starting with functional and, if needed, refactor into a class.
 
 //Remember: React components can show other components
 // but index.js needs a reference to SearchBar in order to import it.
