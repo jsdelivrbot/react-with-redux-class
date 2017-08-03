dc167b87fdc33b58b6716c2601f810713256a93e
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-03 15:26:24 -0600

    1-17 setting up search_bar state

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -11,14 +11,31 @@ import React, { Component } from 'react';
 // the other type of component is a class component:
 // used when we want some type of record keeping and aware of itself and what's happened since it was rendered
 class SearchBar extends Component {
+  // What is state?
+  // State is a plain JS object used to record and react to user events. 
+  // Each class based component that we define has its own state object.
+  // Whenever a component's state is changed, the component re-renders (using its render() method), forcing its children to re-render also (using their render() method).
+  // Only class-based components have a state.
+
+  //This is how we initialize state in a class-based component; must be done in the constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+
   //syntax of class method
   render() {
     // necessary method needed for all class components
     // this method must also return some JSX
-    // return <input onChange={this.onInputChange} />;
-    // when writing JSX and using JS variables (like this.onInputChange) we need to wrap the variables in {}
+    // non-arrow notation:
 
-    //above using arrow notation:
+    // return <input onChange={this.onInputChange} />;
+
+    // when writing JSX and using JS variables (like this.onInputChange above) we need to wrap the variables in {}
+
+    //above using arrow notation: (note: don't need separate defined event handler)
     //-don't need initial curly braces after the arrow if the arrow notation code is all on one line
     //-if have a single argument (like event), we don't need the parentheses around the parameter
     return <input onChange={event => console.log(event.target.value)} />;
@@ -43,3 +60,4 @@ export default SearchBar;
 // const foo = 5;
 // export default foo;
 //then if index.js imported SearchBar, SearchBar would = 5, not the above component
+
