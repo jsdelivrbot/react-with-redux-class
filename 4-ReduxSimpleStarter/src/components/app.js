eff0d817a793b8623e859e2c70bd13dee44a4023
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-30 10:46:56 -0600

    added my redux and react notes into the project

--- 4-ReduxSimpleStarter/src/components/app.js
+++ 4-ReduxSimpleStarter/src/components/app.js
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
   This does not need to be a container because it doesn't really care about the list of books or the currently selected books. The only thing it is here for is to render the book list and book detail to the page; it doesn't care what those show.
 */
 
-import Booklist from '../containers/book-list';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Booklist />
+        <BookList />
       </div>
     );
   }
