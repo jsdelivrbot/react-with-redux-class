f077214551f80150ab2f1ec9875be988fe6873c6
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-30 17:06:39 -0600

    4-47 setup new reducer to consume actions

--- 4-ReduxSimpleStarter/src/reducers/index.js
+++ 4-ReduxSimpleStarter/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 //this tells Redux how to create our application state
 //we have one piece so far: books which gets created from our BooksReducer function
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook,
 });
+// remember any key to the object that we provide to combineReducers, ends up as a key on our global state
 
 export default rootReducer;
