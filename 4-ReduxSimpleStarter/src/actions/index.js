3f79f53159afeb17cacc048c7b75edaa87d6c827
Tyler Grubb <tgrubb@gasbuddy.com>
2017-08-30 15:44:34 -0600

    4-46 setup action and click event

--- 4-ReduxSimpleStarter/src/actions/index.js
+++ 4-ReduxSimpleStarter/src/actions/index.js
@@ -1,7 +1,7 @@
 export function selectBook(book) {
   //console.log('A book has been selected: ', book.title);
 
-  // selectBook is an ActionCreator, it needs to return an action: an object with a type property
+  // selectBook is an ActionCreator, so it needs to return an action: an object with a type property
   return {
     type: 'BOOK_SELECTED',
     payload: book
